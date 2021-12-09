1336d89f650ea9ebcbced37c009299a447bd5c4d
Oleksandr Kryvenko <13kryvenko@gmail.com>
2021-12-09 10:57:18 +0200

    Third commit

--- src/gitdemopack/GitDemoFile.java
+++ src/gitdemopack/GitDemoFile.java
@@ -5,5 +5,6 @@ public class GitDemoFile {
 	public static void main(String[] args) {
 		System.out.println("this is sample file");
 		System.out.println("big butt");
+		System.out.println("big boobies");
 	}
 }
