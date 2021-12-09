26ba15a71d706335e4957209ccabd7b749ba9465
Oleksandr Kryvenko <13kryvenko@gmail.com>
2021-12-09 10:50:12 +0200

    first commit

---
+++ src/gitdemopack/GitDemoFile.java
@@ -0,0 +1,9 @@
+package gitdemopack;
+
+public class GitDemoFile {
+
+	public static void main(String[] args) {
+		System.out.println("this is sample file");
+		System.out.println("big butt");
+	}
+}
